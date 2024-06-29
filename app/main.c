2c42103286548ca1169c6e18f80de3fdee5ad31c
Najib Muhammad <najibm38@mail.com>
2024-06-29 11:14:37 +0100

    The exit builtin

--- app/main.c
+++ app/main.c
@@ -1,8 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 int
-main(void) {
+exit_status(const char *cmd)
+{
+	char *code = (char *)cmd;
+	char c; 
+	int i, len, digit, number = 0;
+
+	len = strlen(code);
+	for(i = 0; i < len; i++) 
+    {
+		c = code[i]; 
+		if(c >= '0' && c <= '9')
+		{
+			digit = c - '0'; 
+			number = number * 10 + digit;
+		}
+    }
+	return (number);
+}
+
+void
+check_exit(const char *cmd)
+{
+	int code = 0;
+	if (!strncmp("exit", cmd, 4)) {
+		code = exit_status(cmd);
+		exit(code);
+	}
+}
+
+int
+main(void)
+{
 	int status = 1;
 
 	while (status)
@@ -19,6 +51,7 @@ main(void) {
 		else
 			status = 0;
 
+		check_exit(input);
 		fprintf(stderr, "%s: command not found\n", input);
 	}
 	return (0);
