1041c0478ecd99e3d4f2855374e99a9d1d686c9b
unknown <Najib Muhammad>
2024-07-01 14:36:31 +0100

    The type builtin: builtins

--- app/main.c
+++ app/main.c
@@ -8,12 +8,12 @@ exit_status(const char *cmd)
 	int i, number = 0;
 
 	for(i = 0; cmd[i]; i++)
-    {
+	{
 		if(cmd[i] >= '0' && cmd[i] <= '9')
 		{
 			number = number * 10 + (cmd[i] - '0');
 		}
-    }
+	}
 	return (number);
 }
 
@@ -28,8 +28,39 @@ check_exit(const char *cmd)
 void
 check_echo(const char *cmd)
 {
-
 	fprintf(stdout, "%s\n", cmd + 5);
+}
+
+void
+check_type(const char *cmd)
+{
+	const char *builtins[] = {
+		".", ":", "[", "alias", "bg", "bind", "break", "builtin", "caller", "cd",
+		"command", "compgen", "complete", "compopt", "continue", "declare", "dirs",
+		"disown", "echo", "enable", "eval", "exec", "exit", "export", "false", "fc",
+		"fg", "getopts", "hash", "help", "history", "jobs", "kill", "let", "local",
+		"logout", "mapfile", "popd", "printf", "pushd", "pwd", "read", "readarray",
+		"readonly", "return", "set", "shift", "shopt", "source", "suspend", "test",
+		"times", "trap", "true", "type", "typeset", "ulimit", "umask", "unalias",
+		"unset", "wait"
+	}, *delim = " ", *cmdlet = strtok((char *)cmd, delim);
+	int i, limit = sizeof(builtins) / sizeof(builtins[0]);
+
+	while (cmdlet != NULL)
+	{
+		for (i = 0; i < limit; i++)
+		{
+			if (strcmp(builtins[i], cmdlet) == 0)
+			{
+				printf("%s is a shell builtin\n", cmdlet);
+				break;
+			}
+		}
+		if (i == limit)
+			printf("%s: not found\n", cmdlet);
+
+		cmdlet = strtok(NULL, delim);
+	}
 }
 
 int
@@ -44,6 +75,11 @@ check_builtin(const char *cmd)
 		check_echo(cmd);
 		return (1);
 	}
+  else if (!strncmp("type", cmd, 4))
+  {
+    check_type(cmd + 5);
+    return (1);
+  }
 	return (0);
 }
 
